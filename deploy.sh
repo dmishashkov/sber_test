32037371360b320c98dfd8884de07cdba61b46d9
Dmi <dmishashkov@gmail.com>
2024-02-08 21:28:09 +0300

    Final

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,5 @@ minikube image load  --overwrite=true my_jenkins:latest
 kubectl apply -f Jenkins/service.yaml && \
 kubectl apply -f Jenkins/storage.yaml && \
 kubectl apply -f Jenkins/permissions.yaml && \
-kubectl apply -f Jenkins/service.yaml && \
 kubectl apply -f Jenkins/jenkins-deploy.yaml
 
